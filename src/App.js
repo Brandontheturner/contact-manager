a215c4c2a848de63d7de07574e6a295fc6f94376
Brandon Turner <brandontheturner@gmail.com>
2019-03-19 11:13:57 -0500

    setting up contact and header components with props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>App Component</h1>
+        <Header branding="Contact Manager" />
+        <Contact
+          name="John Doe"
+          email="bturner@gmail.com"
+          phone="555-555-5555"
+        />
       </div>
     );
   }
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+//functional component
+import React from "react";
+
+const Header = props => {
+  return (
+    <div>
+      <h1>{props.branding}</h1>
+    </div>
+  );
+};
+
+export default Header;
