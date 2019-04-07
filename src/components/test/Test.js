7ff39d085beb0db8cc693f271ea9d12733428f18
Brandon Turner <brandontheturner@gmail.com>
2019-04-06 19:32:50 -0500

    fetch requesr

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -2,29 +2,37 @@ import React, { Component } from "react";
 
 class Test extends Component {
   state = {
-    test: "test"
+    title: "",
+    body: ""
   };
   componentDidMount = () => {
-    console.log("componentDidMount...");
+    fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          title: data.title,
+          body: data.body
+        })
+      );
   };
 
-  componentWillMount = () => {
-    console.log("componentWillMount...");
-  };
-  componentDidUpdate = () => {
-    console.log("componentDidUpdate...");
-  };
-  componentWillUpdate = () => {
-    console.log("componentWillUpdate...");
-  };
-  componentWillReceiveProps = (nextProps, nextState) => {
-    console.log("componentWillRecieveProps...");
-  };
-  static getDerivedStateFromProps(nextProps, prevState) {
-    console.log("getDerivedStateFromProps");
-    return null;
-    //getDerivedStateFromProps requires a return either state or null.
-  }
+  // componentWillMount = () => {
+  //   console.log("componentWillMount...");
+  // };
+  // componentDidUpdate = () => {
+  //   console.log("componentDidUpdate...");
+  // };
+  // componentWillUpdate = () => {
+  //   console.log("componentWillUpdate...");
+  // };
+  // componentWillReceiveProps = (nextProps, nextState) => {
+  //   console.log("componentWillRecieveProps...");
+  // };
+  // static getDerivedStateFromProps(nextProps, prevState) {
+  //   console.log("getDerivedStateFromProps");
+  //   return null;
+  //   //getDerivedStateFromProps requires a return either state or null.
+  // }
 
   //read medium article on lifecycle methods
 
